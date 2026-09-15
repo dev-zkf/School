ce66f2adc20fc7a687c78bbfcc7da405701d84f4
miro <>
2026-09-15 08:46:49 +0300

    Worked on gambling game, need to add a list for the active game buttons for smarter code.

--- ProgrammingStudies/GamblingGame/Form1.Designer.cs
+++ ProgrammingStudies/GamblingGame/Form1.Designer.cs
@@ -31,15 +31,19 @@
             this.GambleBtn = new System.Windows.Forms.Button();
             this.LogText = new System.Windows.Forms.RichTextBox();
             this.spendAmountTxt = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.spendAmountTxt)).BeginInit();
             this.SuspendLayout();
             // 
             // GambleBtn
             // 
             this.GambleBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.GambleBtn.Location = new System.Drawing.Point(261, 274);
+            this.GambleBtn.Location = new System.Drawing.Point(237, 187);
             this.GambleBtn.Name = "GambleBtn";
-            this.GambleBtn.Size = new System.Drawing.Size(193, 63);
+            this.GambleBtn.Size = new System.Drawing.Size(237, 64);
             this.GambleBtn.TabIndex = 0;
             this.GambleBtn.Text = "Gamble";
             this.GambleBtn.UseVisualStyleBackColor = true;
@@ -54,7 +58,7 @@
             this.LogText.Location = new System.Drawing.Point(12, 12);
             this.LogText.Name = "LogText";
             this.LogText.ReadOnly = true;
-            this.LogText.Size = new System.Drawing.Size(442, 239);
+            this.LogText.Size = new System.Drawing.Size(212, 239);
             this.LogText.TabIndex = 2;
             this.LogText.Text = "";
             this.LogText.TextChanged += new System.EventHandler(this.LogText_TextChanged);
@@ -62,22 +66,70 @@
             // spendAmountTxt
             // 
             this.spendAmountTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.spendAmountTxt.Location = new System.Drawing.Point(12, 288);
+            this.spendAmountTxt.Location = new System.Drawing.Point(237, 305);
             this.spendAmountTxt.Maximum = new decimal(new int[] {
             9999,
             0,
             0,
             0});
             this.spendAmountTxt.Name = "spendAmountTxt";
-            this.spendAmountTxt.Size = new System.Drawing.Size(138, 32);
+            this.spendAmountTxt.Size = new System.Drawing.Size(237, 32);
             this.spendAmountTxt.TabIndex = 3;
             this.spendAmountTxt.ValueChanged += new System.EventHandler(this.spendAmountTxt_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 305);
+            this.label1.MaximumSize = new System.Drawing.Size(200, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 32);
+            this.label1.TabIndex = 4;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(237, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 117);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(318, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 117);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(399, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 117);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(466, 349);
+            this.ClientSize = new System.Drawing.Size(486, 349);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.spendAmountTxt);
             this.Controls.Add(this.LogText);
             this.Controls.Add(this.GambleBtn);
@@ -94,6 +146,10 @@
         private System.Windows.Forms.Button GambleBtn;
         private System.Windows.Forms.RichTextBox LogText;
         private System.Windows.Forms.NumericUpDown spendAmountTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- ProgrammingStudies/GamblingGame/Form1.cs
+++ ProgrammingStudies/GamblingGame/Form1.cs
@@ -13,35 +13,42 @@ namespace GamblingGame
 {
     public partial class Form1 : Form
     {
-        private int money;
+        private int money = 100;
         private int spendAmount;
+        private int selectedNumber = -1;
 
         public Form1()
         {
             InitializeComponent();
-            OutputToLog("Casino has opened");
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            // Initial actions
+            OutputToLog("Casino has opened");
+            label1.Text = $"{money.ToString()}€";
         }
 
 
         private void GambleBtn_Click(object sender, EventArgs e)
         {
+            if (selectedNumber == -1) OutputToLog("Select a number first"); return;
+            Random random = new Random();
+            int rng = random.Next(1, 3);
 
+
+            if (selectedNumber == rng)
+            {
+                // Win logic
+                OutputToLog("You won!!!");
+            }
+            else
+            {
+                // Lose logic
+                OutputToLog("You have lost, lol.");
+            }
         }
 
-        private void AmountTxt_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void LogText_TextChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void spendAmountTxt_ValueChanged(object sender, EventArgs e)
         {
@@ -54,6 +61,7 @@ namespace GamblingGame
             {
                 OutputToLog("You are too broke, i have corrected your input to what you have.");
                 spendAmount = money;
+                spendAmountTxt.Value = money;
             }
 
         }
@@ -61,7 +69,52 @@ namespace GamblingGame
         private void OutputToLog(string msg)
         {
             DateTime DT = DateTime.Parse("31/12/2006 07:00:00 AM");
-            LogText.AppendText(Environment.NewLine + DT.ToString("H:mm:ss") + $" {msg}");
+            LogText.AppendText(Environment.NewLine + DT.ToString("H:mm:ss") + $": {msg}");
         }
+
+        private void SetButtonsColor(int btn)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (i != btn) ;
+                    // MAKE A LIST FOR BUTTONS AND ADD THEM TO IT
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (selectedNumber != -1) return; // just making sure it hasnt been already selected
+            selectedNumber = 3;
+            button3.BackColor = Color.Gray;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (selectedNumber != -1) return; // just making sure it hasnt been already selected
+            selectedNumber = 2;
+            button2.BackColor = Color.Gray;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (selectedNumber != -1) return; // just making sure it hasnt been already selected
+            selectedNumber = 1;
+            button1.BackColor = Color.Gray;
+        }
+        private void AmountTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void LogText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
     }
 }
